6f18f93f84ab6dc72a136772bf43bc5421188d35
kma7uwh <manikanta.k@in.verizon.com>
2025-08-11 12:50:06 +0530

    added todo-ui code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+// Interface to define the shape of our Todo object
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-ui';
+
+  // The URL of our Spring Boot backend API
+  private apiUrl = 'http://localhost:8080/api/todos';
+
+  public todos: Todo[] = [];
+  public newTodoTitle: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.http.get<Todo[]>(this.apiUrl).subscribe((data) => {
+      this.todos = data;
+    });
+  }
+
+  addTodo() {
+    if (!this.newTodoTitle.trim()) return; // Don't add empty todos
+
+    const newTodo = { title: this.newTodoTitle, completed: false };
+
+    this.http.post<Todo>(this.apiUrl, newTodo).subscribe((createdTodo) => {
+      this.todos.push(createdTodo);
+      this.newTodoTitle = ''; // Clear the input field
+    });
+  }
 }
